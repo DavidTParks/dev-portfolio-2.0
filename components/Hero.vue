7b5a3b4a87a27337ffdb2fca9fa32b7eea5627e8
David Parks <davidthomasparks@gmail.com>
2020-09-18 15:23:09 -0500

    initial commit'

---
+++ components/Hero.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="relative bg-darkteal overflow-hidden">
+    <div class="hero">
+      <div class="train-wrapper">
+        <Train class="train w-3/12 h-auto"/>
+      </div>
+    </div>
+    <div class="max-w-screen-xl mx-auto ">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Train from '~/assets/svg/train-styled.svg';
+export default {
+  components: {
+    Train
+  }
+}
+</script>
+
+<style>
+.hero {
+  background-image: url('~assets/svg/nolight.svg?inline');
+  background-repeat: no-repeat, no-repeat;
+  background-position: bottom center, 50% -100px;
+  background-size: 100%, 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 400px;
+  margin-bottom: -1px;
+  position: relative;
+}
+
+
+
+.train {
+  margin-left: auto;
+}
+.train-wrapper {
+  position: absolute;
+  bottom: 0;
+  bottom: 0;
+  right: 0;
+  margin-bottom: 1.2%;
+    animation-name: train;
+  animation-duration: 40s;
+  animation-iteration-count: infinite;
+  overflow: visible;
+}
+.train-wrapper:before {
+    content: '';
+    background: #57ffe9;
+    background: radial-gradient(circle, rgba(186, 45, 126, 0.8) 0%, rgba(122, 202, 196, 0) 30%);
+    width: 40%;
+    height: 40%;
+    border-radius: 50%;
+    mix-blend-mode: hard-light;
+    position: absolute;
+    overflow: visible;
+    top: 0;
+    right: 9%;
+    transform: scale(1);
+    animation: pulse 2600ms ease-in-out infinite alternate;
+}
+
+@keyframes pulse {
+  from {
+    transform: scale(1); }
+  to {
+    transform: scale(1.5); } }
+
+@keyframes train {
+  0% {
+    transform: translateX(0);
+  }
+  50% {
+      transform: translateX(-90vw);
+  }
+  100% {
+      transform: translateX(0);
+  }
+}
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        tokyosky: '#00012D',
+        darkteal: '#041628',
+      }
+    }
+  },
+  variants: {},
+  plugins: [],
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js'
+    ]
+  }
+}
