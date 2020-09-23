198fbb7855c3e604936a80a9cc63ff8f0e9ac9fb
David Parks <davidthomasparks@gmail.com>
2020-09-23 18:59:50 -0500

    purge no css

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,7 +28,7 @@ module.exports = {
   ],
   purge: {
     // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-    enabled: process.env.NODE_ENV === 'production',
+    enabled: false,
     content: [
       'components/**/*.vue',
       'layouts/**/*.vue',
