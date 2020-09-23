209f0b388e8a2653c12d7c9d4424da5659d22ff5
David Parks <davidthomasparks@gmail.com>
2020-09-23 18:57:03 -0500

    reinstall node modules

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,6 @@ export default {
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
     "@/assets/css/global.css",
-    "@/assets/css/tailwind.css"
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
@@ -47,8 +46,4 @@ export default {
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
   },
-  tailwindcss: {
-    cssPath: '~/assets/css/tailwind.css',
-  }
-  
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -30,7 +30,6 @@ module.exports = {
     // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
     enabled: process.env.NODE_ENV === 'production',
     content: [
-      'assets/css/tailwind.css',
       'components/**/*.vue',
       'layouts/**/*.vue',
       'pages/**/*.vue',
