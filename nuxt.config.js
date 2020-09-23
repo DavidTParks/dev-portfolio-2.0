020b14809045d63f4b70bc707adf024d341c67ce
David Parks <davidthomasparks@gmail.com>
2020-09-23 18:44:49 -0500

    revert to using tailwind css

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,8 @@ export default {
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
-    "@/assets/css/global.css"
+    "@/assets/css/global.css",
+    "@/assets/css/tailwind.css"
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
@@ -45,5 +46,8 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+  },
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css',
   }
 }
