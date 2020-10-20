eaf166e0c349d2c4d9584460a6765918ab59394b
David Parks <davidthomasparks@gmail.com>
2020-10-19 23:23:28 -0500

    aria label for dinosaur button on article page

--- components/global/DinoArticle.vue
+++ components/global/DinoArticle.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click.prevent="playSound('http://lotl.popapostle.com/sounds/LOTL09/roar.wav')" class="hidden md:inline-block -ml-48 absolute focus:outline-none cvis">
+  <button aria-label="Play dinosaur sound" @click.prevent="playSound('http://lotl.popapostle.com/sounds/LOTL09/roar.wav')" class="hidden md:inline-block -ml-48 absolute focus:outline-none cvis">
       <Dino class="w-32 h-32 dino-article cvis"/>
   </button>
 </template>
